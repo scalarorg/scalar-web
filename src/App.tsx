d396e09ebdc653f19bc41e18640c764b1dd3dab7
LeQuocLap5201 <quoclap5201@gmail.com>
2025-02-18 11:28:55 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="font-bold text-2xl">React + Vite + Tailwind</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
