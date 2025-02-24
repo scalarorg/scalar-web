3aef7d3daf0a835a001eab10be01f950908143c5
LeQuocLap5201 <quoclap5201@gmail.com>
2025-02-24 16:25:21 +0700

    chore: remove @tanstack/router-devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,18 @@
 import { ConnectDropdown } from "@/components/connect";
 import { Footer, Header } from "@/components/layout";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="flex h-screen flex-col">
-        <Header extra={<ConnectDropdown />} />
-        <hr className="shadow-md" />
-        <div className="grow overflow-auto">
-          <div className="container">
-            <Outlet />
-            <Footer />
-          </div>
+    <div className="flex h-screen flex-col">
+      <Header extra={<ConnectDropdown />} />
+      <hr className="shadow-md" />
+      <div className="grow overflow-auto">
+        <div className="container">
+          <Outlet />
+          <Footer />
         </div>
       </div>
-      <TanStackRouterDevtools />
-    </>
+    </div>
   ),
 });
