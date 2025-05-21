e8a89b7ae3140bf66cc3002bcd2594939371a679
LeQuocLap5201 <quoclap5201@gmail.com>
2025-05-21 14:48:29 +0700

    feat(common): add If component for conditional rendering

---
+++ src/components/common/if.tsx
@@ -0,0 +1,30 @@
+import type { PropsWithoutRef, ReactNode } from "react";
+import { useMemo } from "react";
+
+type Condition<Value = unknown> = Value | false | null | undefined | 0 | "";
+
+export function If<Value = unknown>({
+  condition,
+  children,
+  fallback,
+}: PropsWithoutRef<{
+  condition: Condition<Value>;
+  children: ReactNode | ((value: Value) => ReactNode);
+  fallback?: ReactNode;
+}>) {
+  return useMemo(() => {
+    if (condition) {
+      if (typeof children === "function") {
+        return <>{children(condition)}</>;
+      }
+
+      return <>{children}</>;
+    }
+
+    if (fallback) {
+      return <>{fallback}</>;
+    }
+
+    return null;
+  }, [condition, fallback, children]);
+}
