4379bdd2e0e2aae831d956d2a070b048ce2bd486
LeQuocLap5201 <quoclap5201@gmail.com>
2025-05-23 10:47:45 +0700

    refactor(config): simplify network configuration and retrieval logic

--- src/config/nework.config.ts
+++ src/config/nework.config.ts
@@ -1,4 +1,4 @@
-import { Network } from '@/lib/wallet';
+import { Network } from "@/lib/wallet";
 
 const mempoolApiUrl = import.meta.env.VITE_MEMPOOL_API;
 
@@ -11,64 +11,59 @@ export interface NetworkConfig {
 }
 
 const mainnetConfig: NetworkConfig = {
-  coinName: 'BTC',
-  coinSymbol: 'BTC',
-  networkName: 'BTC',
+  coinName: "BTC",
+  coinSymbol: "BTC",
+  networkName: "BTC",
   mempoolApiUrl,
   network: Network.MAINNET
 };
 
 const testnetConfig: NetworkConfig = {
-  coinName: 'Testnet BTC',
-  coinSymbol: 'tBTC',
-  networkName: 'BTC testnet',
+  coinName: "Testnet BTC",
+  coinSymbol: "tBTC",
+  networkName: "BTC testnet",
   mempoolApiUrl: `${mempoolApiUrl}/testnet`,
   network: Network.TESTNET
 };
 
 const testnet4Config: NetworkConfig = {
-  coinName: 'Testnet BTC',
-  coinSymbol: 'tBTC',
-  networkName: 'BTC testnet4',
+  coinName: "Testnet BTC",
+  coinSymbol: "tBTC",
+  networkName: "BTC testnet4",
   mempoolApiUrl: `${mempoolApiUrl}/testnet4`,
   network: Network.TESTNET4
 };
 
 const regtestConfig: NetworkConfig = {
-  coinName: 'Regtest BTC',
-  coinSymbol: 'rBTC',
-  networkName: 'BTC regtest',
+  coinName: "Regtest BTC",
+  coinSymbol: "rBTC",
+  networkName: "BTC regtest",
   mempoolApiUrl,
   network: Network.REGTEST
 };
 
 const config: Record<Network, NetworkConfig> = {
-  mainnet: mainnetConfig,
-  testnet: testnetConfig,
-  testnet4: testnet4Config,
-  regtest: regtestConfig
+  [Network.MAINNET]: mainnetConfig,
+  [Network.TESTNET]: testnetConfig,
+  [Network.TESTNET4]: testnet4Config,
+  [Network.REGTEST]: regtestConfig
 };
 
 export function getNetworkConfig(network = Network.TESTNET4): NetworkConfig {
-  switch (network) {
-    case Network.MAINNET:
-      return config.mainnet;
-    case Network.TESTNET:
-      return config.testnet;
-    case Network.TESTNET4:
-      return config.testnet4;
-    case Network.REGTEST:
-      return config.regtest;
-    default:
-      throw new Error(`Unsupported network: ${network}`);
+  const result = config[network];
+
+  if (!result) {
+    throw new Error(`Unsupported network: ${network}`);
   }
+
+  return result;
 }
 
 export function validateAddress(network: Network, address: string): void {
-  if (network === Network.MAINNET && !address.startsWith('bc1')) {
+  if (network === Network.MAINNET && !address.startsWith("bc1")) {
     throw new Error("Incorrect address prefix for Mainnet. Expected address to start with 'bc1'.");
   }
-  if ([Network.TESTNET, Network.TESTNET4].includes(network) && !address.startsWith('tb1')) {
+  if ([Network.TESTNET, Network.TESTNET4].includes(network) && !address.startsWith("tb1")) {
     throw new Error("Incorrect address prefix for Testnet / Signet. Expected address to start with 'tb1'.");
   }
   if (![Network.MAINNET, Network.TESTNET, Network.TESTNET4].includes(network)) {
